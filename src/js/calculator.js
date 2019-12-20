9ec81e6031dde2fe9695371bfc5586474ee6d168
krystianbialy <krystian.bialy@onet.pl>
2019-12-20 12:36:30 +0100

    adding variables and fixes

---
+++ src/js/calculator.js
@@ -0,0 +1,50 @@
+var buttonOne = document.getElementById(
+  'calculator__input-operation-value-one'
+);
+var buttonTwo = document.getElementById(
+  'calculator__input-operation-value-two'
+);
+var buttonThree = document.getElementById(
+  'calculator__input-operation-value-three'
+);
+var buttonFour = document.getElementById(
+  'calculator__input-operation-value-four'
+);
+var buttonFive = document.getElementById(
+  'calculator__input-operation-value-five'
+);
+var buttonSix = document.getElementById(
+  'calculator__input-operation-value-six'
+);
+var buttonSeven = document.getElementById(
+  'calculator__input-operation-value-seven'
+);
+var buttonEight = document.getElementById(
+  'calculator__input-operation-value-eight'
+);
+var buttonNine = document.getElementById(
+  'calculator__input-operation-value-nine'
+);
+var buttonZero = document.getElementById(
+  'calculator__input-operation-value-zero'
+);
+
+var buttonClear = document.getElementById(
+  'calculator__input-operation-value-clear'
+);
+var buttonBackspace = document.getElementById(
+  'calculator__input-operation-value-backspace'
+);
+var buttonComma = document.getElementById(
+  'calculator__input-operation-value-comma'
+);
+var outputOperationValue = document.getElementById(
+  'calculator__output-operation-value'
+);
+
+var allNumbers = document.getElementsByClassName(
+  'calculator__input-operation-value--number'
+);
+var allOperators = document.getElementsByClassName(
+  'calculator__input-operation-value--operator'
+);
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './main.scss';
+import './js/calculator.js';
 
 import 'mdbootstrap/scss/mdb-lite-free.scss';
 import 'mdbootstrap/js/mdb.min.js';
