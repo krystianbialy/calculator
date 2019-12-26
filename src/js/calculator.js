7ebe5c2aa504f94e738da7f49c543b364c84c2fa
krystianbialy <krystian.bialy@onet.pl>
2019-12-26 16:01:45 +0100

    work on logic & variables fixes

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,50 +1,121 @@
-var buttonOne = document.getElementById(
-  'calculator__input-operation-value-one'
+const allNumbers = document.getElementsByClassName(
+  'calculator__input-operation-value--number'
 );
-var buttonTwo = document.getElementById(
-  'calculator__input-operation-value-two'
+const allOperators = document.getElementsByClassName(
+  'calculator__input-operation-value--operator'
 );
-var buttonThree = document.getElementById(
-  'calculator__input-operation-value-three'
-);
-var buttonFour = document.getElementById(
-  'calculator__input-operation-value-four'
-);
-var buttonFive = document.getElementById(
-  'calculator__input-operation-value-five'
-);
-var buttonSix = document.getElementById(
-  'calculator__input-operation-value-six'
-);
-var buttonSeven = document.getElementById(
-  'calculator__input-operation-value-seven'
-);
-var buttonEight = document.getElementById(
-  'calculator__input-operation-value-eight'
-);
-var buttonNine = document.getElementById(
-  'calculator__input-operation-value-nine'
-);
-var buttonZero = document.getElementById(
-  'calculator__input-operation-value-zero'
-);
-
-var buttonClear = document.getElementById(
-  'calculator__input-operation-value-clear'
-);
-var buttonBackspace = document.getElementById(
-  'calculator__input-operation-value-backspace'
-);
-var buttonComma = document.getElementById(
+const buttonComma = document.getElementById(
   'calculator__input-operation-value-comma'
 );
+const buttonBackspace = document.getElementById(
+  'calculator__input-operation-value-backspace'
+);
+const buttonClear = document.getElementById(
+  'calculator__input-operation-value-clear'
+);
+
 var outputOperationValue = document.getElementById(
   'calculator__output-operation-value'
 );
 
-var allNumbers = document.getElementsByClassName(
-  'calculator__input-operation-value--number'
-);
-var allOperators = document.getElementsByClassName(
-  'calculator__input-operation-value--operator'
-);
+var displayValue = 0;
+var pendingValue;
+var calaculationsZone = [];
+
+let loadDisplayValue = () => {
+  outputOperationValue.innerHTML = displayValue;
+};
+
+loadDisplayValue();
+
+let updateOutputOperationValue = itemClicked => {
+  let buttonValue = itemClicked.target.innerText;
+
+  if (displayValue === 0) {
+    displayValue = '';
+  }
+
+  displayValue += buttonValue;
+  outputOperationValue.innerHTML = displayValue.replace('.', ',');
+};
+
+let performOperation = itemClicked => {
+  let operator = itemClicked.target.innerText;
+
+  switch (operator) {
+    case '+':
+      pendingValue = displayValue;
+      displayValue = 0;
+      outputOperationValue.innerHTML = displayValue;
+      calaculationsZone.push(pendingValue);
+      calaculationsZone.push('+');
+      console.log(calaculationsZone);
+      break;
+
+    case '-':
+      pendingValue = displayValue;
+      displayValue = 0;
+      outputOperationValue.innerHTML = displayValue;
+      calaculationsZone.push(pendingValue);
+      calaculationsZone.push('-');
+      break;
+
+    case '*':
+      pendingValue = displayValue;
+      displayValue = 0;
+      outputOperationValue.innerHTML = displayValue;
+      calaculationsZone.push(pendingValue);
+      calaculationsZone.push('*');
+      break;
+
+    case '/':
+      pendingValue = displayValue;
+      displayValue = 0;
+      outputOperationValue.innerHTML = displayValue;
+      calaculationsZone.push(pendingValue);
+      calaculationsZone.push('/');
+      break;
+
+    case '=':
+      calaculationsZone.push(displayValue);
+      let test = calaculationsZone.join('').replace(/,/g, '.');
+      console.log(test);
+      let resultOfTheAction = eval(test);
+      console.log(resultOfTheAction);
+      displayValue = resultOfTheAction + '';
+      outputOperationValue.innerHTML = displayValue.replace('.', ',');
+      calaculationsZone = [];
+      break;
+  }
+};
+
+for (let i = 0; i < allNumbers.length; i += 1) {
+  allNumbers[i].addEventListener('click', updateOutputOperationValue);
+}
+
+for (let i = 0; i < allOperators.length; i += 1) {
+  allOperators[i].addEventListener('click', performOperation);
+}
+
+buttonBackspace.onclick = () => {
+  let displayValueLength = displayValue.length;
+  displayValue = displayValue.slice(0, displayValueLength - 1);
+  outputOperationValue.innerHTML = displayValue.replace('.', ',');
+
+  if (displayValue === '') {
+    displayValue = 0;
+    outputOperationValue.innerHTML = displayValue;
+  }
+};
+
+buttonClear.onclick = () => {
+  displayValue = 0;
+  outputOperationValue.innerHTML = displayValue;
+};
+
+buttonComma.onclick = () => {
+  if (!displayValue.includes(',')) {
+    displayValue += ',';
+    outputOperationValue.innerHTML = displayValue;
+  }
+};
